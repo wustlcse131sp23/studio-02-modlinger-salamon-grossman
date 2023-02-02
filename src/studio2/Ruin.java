bbb88c728652281ff45f6dc934111f705fc2dc65
jacksalamon <jacksalamon@localhost>
2023-02-02 12:46:56 -0600

    Completed Ruin

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -20,52 +20,66 @@ public class Ruin {
 		System.out.println("what is your total simulations you want to run?");
 		int totalSimulations = in.nextInt();
 		boolean coinFlip;
-		int simulations = 1;
+		int simulations = 0;
 		int rounds = 0;
+		int temp = startAmount;
+		int loss = 0;
+		String result;
+		double expectedRuin;
+		double alpha;
+		alpha = (1-winChance)/winChance;
+	
 		
-		while(simulations <= totalSimulations) {
-		
+		while(simulations < totalSimulations) {
+			
 			rounds = 0;
+			simulations++;
+			startAmount = temp;
 			
-		while(startAmount < winLimit) {
+			while((startAmount < winLimit) && (startAmount > 0)) {
 			
-			if(startAmount > 0) {
-				
 				coinFlip = Math.random() < winChance;
-				
-				System.out.println (coinFlip);
 				
 				if (coinFlip) {
 					
 					startAmount++;
-					System.out.println (startAmount);
-					rounds++;
+					//System.out.println(startAmount);
 					
-				} else if (!coinFlip){
+				} else {
 					
 					startAmount--;
-					System.out.println (startAmount);
-					rounds++;
 					
 				}
-				
+			
+				rounds++;
+			
+			}
+		
+			if(startAmount == winLimit) {
+			
+				result = "win";
+			
 			} else {
+			
+				result = "lose";
+				loss++;
 				
-				System.out.println("You Lose!");
-				
+			
 			}
 			
-			
-			System.out.println("Simulation " + simulations + ": " + rounds);
-		}
-		System.out.println("Thanks for playing you win!");
-		System.out.println("Simulation " + simulations + ": ");
+			System.out.println("Simulation " + simulations + ": " + rounds + " " + result);
 		
 		}
 		
+		System.out.println("You lost " + loss + " times!");
 		
+		if (winChance == 0.5) {
+			expectedRuin = 1 - (startAmount/winLimit);
+		} else {
+			expectedRuin = ((Math.pow(alpha,startAmount))-(Math.pow(alpha, winLimit)))/(1-(Math.pow(alpha,winLimit)));
+		}
 		
-		
+		System.out.println(expectedRuin);
 		
 	}
 
